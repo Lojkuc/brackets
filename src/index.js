84aa51ada3bc935bdd7a5ec749da42c3080fbc64
Vladislav Lojkuc <lojkucvladislav@gmail.com>
2022-08-02 19:43:35 +0300

    feat:add full code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = []
+  let brackets ={
+    ")":"(",
+    "}":"{",
+    "]":"[",
+  }
+  for (let i = 0; i < str.length; i++) {
+  const current = str[i]
+    if(isClosedBracket(current)){
+      if(brackets[current] !== stack.pop()) return false
+    } else{
+      stack.push(current)
+    }
+  }
+  return stack.length == 0;
+}
+function isClosedBracket(char){
+    return [")","}","]"].indexOf(char)>-1
 }
